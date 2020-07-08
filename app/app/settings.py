27277028b2f00dc46e0205c47a18b1dc049bb344
INTERNET\AD01 <insightart.team@gmail.com>
2020-07-08 13:07:57 +0300

    creare template pentru pagini - in general

--- app/app/settings.py
+++ app/app/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates"),
+                os.path.join(BASE_DIR, "bookstore_app//templates//bookstore_app"),
+                ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
